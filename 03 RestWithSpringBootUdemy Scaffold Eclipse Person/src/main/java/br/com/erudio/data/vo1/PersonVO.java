b19df92a0d778333c1df0f52b18f23c5d95a64a0
Daniel da Silva de Lima <dsl15021996@gmail.com>
2019-09-24 17:40:23 -0300

    voltei para o curso

--- 03 RestWithSpringBootUdemy Scaffold Eclipse Person/src/main/java/br/com/erudio/data/vo1/PersonVO.java
+++ 03 RestWithSpringBootUdemy Scaffold Eclipse Person/src/main/java/br/com/erudio/data/vo1/PersonVO.java
@@ -1,15 +1,24 @@
-package br.com.erudio.data.vo;
+package br.com.erudio.data.vo1;
 
 import java.io.Serializable;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonPropertyOrder;
+
+@JsonPropertyOrder({"id", "address", "firstName", "lastName", "gender"})
 public class PersonVO implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
 	
 	private long id;
+	
+	@JsonProperty("first_name")
 	private String firstName;
+	@JsonProperty("last_name")
 	private String lastName;
 	private String address;
+	@JsonIgnore
 	private String gender;
 	
 	
